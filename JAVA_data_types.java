382c2cad04539276f56e3f9edd012f07b8cdd961
RANJIT VITTHAL AIWALE <72188933+ranjitaiwale@users.noreply.github.com>
2021-08-04 16:10:48 +0530

    Add files via upload

---
+++ JAVA_data_types.java
@@ -0,0 +1,41 @@
+public class JAVA_data_types {
+    public static void main(String[]args){
+/*
+*****************************|| data types and variables || ******************************
+* Binary system
+* 1 bit =  1 or 0 = 2 possible outcome
+* 2 bit = 2 raise to 2 = 4
+* 3 bit 2 raise to 3 = 8
+* n bit 2 raise to n = 2 raise to n
+*  1byte = 8 bits ==256 number can be stored
+*
+*
+* 1 byte = 8 bits means 0 to 256 number but -128 to 127
+* 1st bit means most significant beat means 1 0r 0 1 means -ve and 0 means positive
+* means 1 bit sign dyayal gel mhan 7 bit rahil mhanun 2 raise to 7 mhanje 7 bit made 128
+* number store karu shakto.
+*
+* int =  4 bytes
+* short =  2 bytes
+* long =  8 bytes
+*
+*
+****************** variables ********************
+*int age = 10;
+* variable name = age
+* data type = int
+* value = 10
+*
+*
+ * */
+int age = 10;
+
+float rateOfIntrest=2.3f;
+double rate = 3.43394834;
+boolean isThisSeriousCool = true;
+char myCharacter = 'a';
+        System.out.println( "age = "+age+ " \n rateOfIntrest = "
+                +rateOfIntrest + " \n rate"+rate + "\n isThisSeriousCool" +
+                isThisSeriousCool + " \n myCharacter "+ myCharacter);
+    }
+}
